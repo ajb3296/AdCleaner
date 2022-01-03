f4b71218304fbc7b1717ce8e9cf6f00ca361e164
An Jaebeom <ajb8533296@gmail.com>
2022-01-04 02:24:41 +0900

    add host file info

--- AdCleaner.py
+++ AdCleaner.py
@@ -230,6 +230,17 @@ if __name__ == '__main__':
         hostsfilepath = "/etc/hosts"
 
     elif platform_sys == "Darwin":
+        """
+        ##
+        # Host Database
+        #
+        # localhost is used to configure the loopback interface
+        # when the system is booting.  Do not change this entry.
+        ##
+        127.0.0.1	localhost
+        255.255.255.255	broadcasthost
+        ::1             localhost
+        """
         if os.path.exists("/etc/hosts"):
             hostsfilepath = "/etc/hosts"
         else:
